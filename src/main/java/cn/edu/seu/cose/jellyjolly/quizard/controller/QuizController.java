58ae02108cdabb10c9a62fd004a7a971cf997022
王建双 <iostreamin@gmail.com>
2013-03-17 00:16:33 +0800

    some controller

--- src/main/java/cn/edu/seu/cose/jellyjolly/quizard/controller/QuizController.java
+++ src/main/java/cn/edu/seu/cose/jellyjolly/quizard/controller/QuizController.java
@@ -42,6 +42,15 @@ public class QuizController {
 
     private QuizService quizService;
     private int maxQuizCount = 25;
+    private int maxQuizCountOfAdmin = 15;
+
+    public int getMaxQuizCountOfAdmin() {
+        return maxQuizCountOfAdmin;
+    }
+
+    public void setMaxQuizCountOfAdmin(int maxQuizCountOfAdmin) {
+        this.maxQuizCountOfAdmin = maxQuizCountOfAdmin;
+    }
 
     public QuizController(QuizService quizService) {
         this.quizService = quizService;
